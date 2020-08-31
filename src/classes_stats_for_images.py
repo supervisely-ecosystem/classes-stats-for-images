784efcb0de7393dcfc7980d544f44b0e4c398483
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-08-31 18:04:33 +0300

    pascal report in progress

--- src/classes_stats_for_images.py
+++ src/classes_stats_for_images.py
@@ -4,6 +4,7 @@ import random
 from collections import defaultdict
 import pandas as pd
 import json
+import numpy as np
 # <!--    <sly-notification content="<Текст нотификации в маркдауне>" :options="{ type: 'note|info|warning|error', name: '<заголовок нотификации>'}" />-->
 
 
@@ -14,6 +15,7 @@ WORKSPACE_ID = int(os.environ['modal.state.workspaceId'])
 PROJECT_ID = os.environ.get('modal.state.inputProjectId', None)
 DATASET_ID = os.environ.get('modal.state.inputDatasetId', None)
 SAMPLE_PERCENT = int(os.environ['modal.state.samplePercent'])
+BG_COLOR = [0, 0, 0]
 
 
 def sample_images(api, datasets):
@@ -30,6 +32,8 @@ def sample_images(api, datasets):
     ds_images = defaultdict(list)
     for image_info in all_images:
         ds_images[image_info.dataset_id].append(image_info)
+    return ds_images
+
 
 @sly.timeit
 @my_app.callback("calc")
@@ -50,14 +54,61 @@ def calc(api: sly.Api, task_id, context, state, app_logger):
     meta = sly.ProjectMeta.from_json(meta_json)
     colors_warning = meta.obj_classes.validate_classes_colors()
 
+    # list classes
+    class_names = []
+    class_colors = []
+    class_indices = []  # 0 - for unlabeled area
+    class_indices_colors = []
+    _name_to_index = {}
+    for idx, obj_class in enumerate(meta.obj_classes):
+        class_names.append(obj_class.name)
+        class_colors.append(obj_class.color)
+        class_index = idx + 1
+        class_indices.append(class_index)
+        class_indices_colors.append([class_index, class_index, class_index])
+        _name_to_index[obj_class.name] = class_index
 
-
+    ds_images = sample_images(datasets)
     all_stats = []
     for dataset_id, images in ds_images.items():
         for batch in sly.batched(images):
-            stats = []
-            for image_info in batch:
-                pass
+            image_ids = [image_info.id for image_info in batch]
+            image_names = [image_info.name for image_info in batch]
+
+            ann_infos = api.annotation.download_batch(dataset_id, image_ids)
+            ann_jsons = [ann_info.annotation for ann_info in ann_infos]
+
+            for info, ann_json in zip(batch, ann_jsons):
+                ann = sly.Annotation.from_json(ann_json, meta)
+
+                render_idx_rgb = np.zeros(ann.img_size + (3,), dtype=np.uint8)
+                render_idx_rgb[:] = BG_COLOR
+                ann.draw_class_idx_rgb(render_idx_rgb, _name_to_index)
+                temp_area = sly.Annotation.stat_area(render_idx_rgb, class_names, class_indices_colors)
+
+                x = 10
+                # temp_count = ann.stat_class_count(class_names)
+                # if len(tag_names) != 0:
+                #     temp_img_tags = ann.stat_img_tags(tag_names)
+                #
+                # temp_area['id'] = info.id
+                # temp_area['dataset'] = dataset.name
+                # temp_area['name'] = '<a href="{0}" rel="noopener noreferrer" target="_blank">{1}</a>' \
+                #     .format(api.image.url(team.id,
+                #                           workspace.id,
+                #                           project.id,
+                #                           dataset.id,
+                #                           info.id),
+                #             info.name)
+                #
+                # stats_area.append(temp_area)
+                # stats_count.append(temp_count)
+                # if len(tag_names) != 0:
+                #     stats_img_tags.append(temp_img_tags)
+
+            # ds_progress.iters_done_report(len(batch))
+            # total_images_in_project += len(batch)
+
 
 def main():
     sly.logger.info("Script arguments", extra={"teamId: ": TEAM_ID, "workspaceId: ": WORKSPACE_ID,
